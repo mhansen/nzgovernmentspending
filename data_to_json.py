50b6be2daea4931c4e461757308c4c0ef6736b70
Mark Hansen <mark@markhansen.co.nz>
2011-05-10 11:14:27 +1200

    Convert expenses data to JSON format for the client.

---
+++ data_to_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Converts tab separated fields into JSON ready to be eaten by javascript.
+"""
+import sys
+import json
+import re
+import collections
+
+def factory():
+    return collections.defaultdict(factory)
+
+data = collections.defaultdict(factory)
+
+for line in sys.stdin:
+    fields = line.split("\t")
+    unitname = fields[0]
+    description = fields[1]
+    amount = int(fields[2].rstrip().lstrip().replace(",",""))
+    year = int(fields[3].rstrip())
+    
+    data[year][unitname][description] = amount
+
+print json.dumps(data)
