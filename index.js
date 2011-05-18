002eccd888cb5f0a45372dc2430f42dadf4dd51d
Mark Hansen <mark@markhansen.co.nz>
2011-05-18 15:14:15 +1200

    Add in detail view

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 var NZ_POPULATION = 4405193;
+var YEAR = 2011;
+var raw_expenses;
+var main_chart;
+var detail_chart;
+var expense_series_by_dept = {};
 
 $(function () {
     $.getJSON("expenses.json", function (data) {
-        var currentExpenses = data[2011];
-        var treatedData = [];
-        $.each(currentExpenses, function (name, deptExpenses) {
-            var sum = 0;
-            $.each(deptExpenses, function (subdeptname, cost) {
-                sum += cost;
+        raw_expenses = data;
+
+        $.each(data[YEAR], function (dept_name, dept_expenses) {
+            expense_series_by_dept[dept_name] = [];
+            $.each(dept_expenses, function (subdept_name, subdept_expense) {
+                expense_series_by_dept[dept_name].push([subdept_name, subdept_expense]);
             });
-            treatedData.push([name, sum]);
+            expense_series_by_dept[dept_name].sort(function (a, b) {
+                return b[1] - a[1];
+            });
         });
-        treatedData.sort(function (a, b) {
-            return b[1] - a[1];
-        });
-        plot(treatedData);
+
+        plot(expense_series_for_all_depts(data[YEAR]));
     });
 });
 
-function plot(data) {
-    var chart;
-    chart = new Highcharts.Chart({
+function expense_series_for_all_depts(yearData) {
+    var expense_series = [];
+    $.each(yearData, function (name, dept_expenses) {
+        var sum = 0;
+        $.each(dept_expenses, function (subdept_name, cost) {
+            sum += cost;
+        });
+        expense_series.push([name, sum]);
+    });
+    expense_series.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+    return expense_series;
+}
+
+function plot(depts_data) {
+    main_chart = new Highcharts.Chart({
         chart: {
-            animation: false,
             renderTo: 'chart_container',
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            spacingTop: 0,
-            spacingLeft: 0,
-            spacingRight: 0,
-            spacingBottom: 0
         },
         credits: {
             text: 'markhansen.co.nz/nzgovernmentspending',
@@ -64,8 +75,12 @@ function plot(data) {
                 },
                 point: {
                     events: {
-                        click: function (event) {
-                            alert("You clicked: " + this.name);
+                        select: function (event) {
+                            plot_detail(expense_series_by_dept[this.name]);
+                            console.log(this);
+                            console.log(event);
+                        },
+                        unselect: function (event) {
                         }
                     }
                 },
@@ -78,7 +93,27 @@ function plot(data) {
         series: [{
             type: 'pie',
             name: 'Government Expenses',
-            data: data
+            data: depts_data
         }]
+    });
+}
+
+function plot_detail(dept_data) {
+    if (detail_chart) {
+        detail_chart.destroy();
+    }
+    detail_chart = new Highcharts.Chart({
+        chart: {
+            renderTo: "detail_container"
+        },
+        series: [{
+            type: 'pie',
+            name: 'Detail Expenses',
+            data: dept_data
+        }],
+        plotOptions: {
+            pie: {
+            }
+        }
     });
 }
