6971afd29e1d85a4be9fd9aa16bef6751ee09712
Mark Hansen <mark@markhansen.co.nz>
2011-05-19 21:14:48 +1200

    a day of iteration without version control. :-/

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var raw_expenses;
+var estimates_2011;
+var estimates_2012;
 var main_chart;
 var detail_chart;
 var expense_series_by_dept = {};
+var total_expenses = 0;
 
 function dollars_per_person(dollars_per_country_thousands) {
     var NZ_POPULATION = 4405193; // From Statistics NZ Population Counter.
@@ -9,20 +11,31 @@ function dollars_per_person(dollars_per_country_thousands) {
 }
 
 $(function () {
-    $.getJSON("expenses-2011.json", function (budget) {
-        raw_expenses = budget;
+    $.getJSON("expenses-2011and2012.json", function (budget) {
+        estimates_2011 = budget[2011];
+        estimates_2012 = budget[2012];
 
-        $.each(raw_expenses, function (dept_name, dept_expenses) {
+        $.each(estimates_2012, function (dept_name, dept_expenses) {
             expense_series_by_dept[dept_name] = [];
             $.each(dept_expenses, function (subdept_name, subdept_expense) {
-                expense_series_by_dept[dept_name].push([subdept_name, subdept_expense]);
+                var lastYearExpense;
+                if (estimates_2011[dept_name]) {
+                    lastYearExpense = estimates_2011[dept_name][subdept_expense]
+                }
+
+                expense_series_by_dept[dept_name].push({
+                    name: subdept_name, 
+                    y: subdept_expense,
+                    lastYearY: lastYearExpense
+                });
+                total_expenses += subdept_expense;
             });
             expense_series_by_dept[dept_name].sort(function (a, b) {
-                return b[1] - a[1];
+                return b['y'] - a['y'];
             });
         });
 
-        plot(expense_series_for_all_depts(raw_expenses));
+        plot(expense_series_for_all_depts(estimates_2012));
     });
 });
 
@@ -51,7 +64,7 @@ function plot(depts_data) {
             enabled: false
         },
         title: {
-            text: 'All Departments',
+            text: 'Government Expenses [Budget 2011]',
             margin: 20,
             style: {
                 "fontSize": "16px",
@@ -82,6 +95,7 @@ function plot(depts_data) {
                         select: function (event) {
                             plot_detail_pie(this.name);
                             fill_detail_receipt(this.name);
+                            $.ajax("/gen204?" + this.name)
                         },
                         unselect: function (event) {
                         }
@@ -99,7 +113,6 @@ function plot(depts_data) {
             size: "100%"
         }]
     });
-    console.log(main_chart)
 }
 
 function fill_detail_receipt(dept_name) {
@@ -111,8 +124,8 @@ function fill_detail_receipt(dept_name) {
     $.each(dept_data, function (i, subdept) {
 
         $("<tr class='lineitem'>").
-            append($("<td class='expense'>").text(dollars_per_person(subdept[1]))).
-            append($("<td class='description'>").text(subdept[0])).
+            append($("<td class='expense'>").text(dollars_per_person(subdept['y']))).
+            append($("<td class='description'>").text(subdept['name'])).
                 appendTo($list);
     });
 }
@@ -160,9 +173,15 @@ function plot_detail_pie(dept_name) {
     });
 }
 
+$(function () {
+    $("a#inline").fancybox();
+});
+
 function format_tooltip() {
-    var perperson = dollars_per_person(this.y) + " per person.";
-    var total = "$" + (this.y / 1000000).toFixed(2) + " Billion";
+    console.log(this);
+    var perperson = dollars_per_person(this.y) + " per capita.";
+    var total = "$" + (this.y / 1000000).toFixed(2) + " Billion ";
     var splitName = '<b>'+this.point.name.split(" - ").join("<br/><b>");
-    return splitName +'<br/>'+ total + '<br/>' + perperson;
+    var percentage = "<i>(" +((this.y / total_expenses) * 100).toFixed(2) + "% of total)</i>";
+    return splitName +'<br/>'+ total + percentage + '<br/>' + perperson;
 }
