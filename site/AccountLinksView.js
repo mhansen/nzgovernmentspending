0b7277ad63294c4d65e368c5a091424bd7b80562
Mark Hansen <mark@markhansen.co.nz>
2018-03-22 20:14:39 +1100

    Remove jQuery dependency on one file

--- site/AccountLinksView.js
+++ site/AccountLinksView.js
@@ -1,9 +1,9 @@
 window.AccountLinksView = Backbone.View.extend({
   render(viewingIncome) {
     if (viewingIncome) {
-      return $(this.el).html("<a href='/?income=false'>Expenses</a> | <b>Incomes</b>");
+      return this.el.innerHTML = "<a href='/?income=false'>Expenses</a> | <b>Incomes</b>";
     } else {
-      return $(this.el).html("<b>Expenses</b> | <a href='/?income=true'>Incomes</a>");
+      return this.el.innerHTML = "<b>Expenses</b> | <a href='/?income=true'>Incomes</a>";
     }
   }
 });
